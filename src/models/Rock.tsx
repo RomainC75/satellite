e390a68f12d8d296dfd51e78b2e907779f06cec1
RomainC75 <rom.chenard@gmail.com>
2024-01-10 22:55:23 +0100

    wip

--- src/models/Rock.tsx
+++ src/models/Rock.tsx
@@ -1,5 +1,7 @@
 import { useGLTF } from "@react-three/drei";
 import rockScene from "../assets/3d/moon_rock.glb";
+import { useFrame } from "@react-three/fiber";
+import { useRef } from "react";
 
 interface IRockProps {
   position: [number, number, number];
@@ -10,9 +12,17 @@ interface IRockProps {
 }
 
 const Rock = (props: IRockProps) => {
-  const { nodes, materials } = useGLTF(rockScene)
+  const { nodes, materials } = useGLTF(rockScene);
+  const ref = useRef();
+
+  useFrame((_, delta) => {
+    ref.current.rotation.y += 0.25 * delta;
+    ref.current.rotation.x += 0.2 * delta;
+    ref.current.rotation.z += 0.3 * delta;
+  });
+
   return (
-    <group {...props} dispose={null}>
+    <group {...props} dispose={null} ref={ref}>
       <mesh
         // castShadow
         // receiveShadow
@@ -21,9 +31,15 @@ const Rock = (props: IRockProps) => {
         // position={[-2.3, -15.27, -38.2]}
         // scale
       />
+      <meshStandardMaterial 
+        metalness={1} 
+        color={"grey"} 
+        opacity={1}
+        transparent={false}
+      />
     </group>
   );
-}
+};
 
 // useGLTF.preload("/moon_rock_2.glb");
 export default Rock;
--- src/pages/home/Home.page.tsx
+++ src/pages/home/Home.page.tsx
@@ -8,17 +8,17 @@ import { RockDetails } from "../../models/RockDetails";
 import Sky from "../../models/Stars";
 
 const Home = () => {
-  const [isRotating, setIsRotating] = useState(false)
+  const [isRotating, setIsRotating] = useState(false);
 
   const adjustRockForScreenSize = (): [
     [number, number, number],
     [number, number, number],
-    [number, number, number],
-  ]=>{
+    [number, number, number]
+  ] => {
     let screenScale: [number, number, number] | null = null;
-    
+
     const screenPosition: [number, number, number] = [0, 0, -43];
-    const rotation: [number, number, number] = [0.1, 4.7, 0]
+    const rotation: [number, number, number] = [0.1, 4.7, 0];
 
     if (window.innerWidth < 768) {
       screenScale = [5, 5, 5];
@@ -26,27 +26,30 @@ const Home = () => {
       screenScale = [7, 7, 7];
     }
 
-    return [ screenScale, screenPosition, rotation];
-  }
-  const [islandScale, islandPosition, islandRotation] = adjustRockForScreenSize();
-
+    return [screenScale, screenPosition, rotation];
+  };
+  const [islandScale, islandPosition, islandRotation] =
+    adjustRockForScreenSize();
 
   return (
     <section className="w-full h-screen relative">
+      <Canvas
+        className={`w-full h-screen bg-transparent ${
+          isRotating ? "cursor-grabbing" : "cursor-grab"
+        }`}
+        camera={{ near: 0.1, far: 2000 }}
+        // style={{ background: "#000000" }}
+      >
+        <Infos />
 
-      <Canvas className={`w-full h-screen bg-transparent ${isRotating ? "cursor-grabbing" : "cursor-grab"}`}
-        camera={{ near: 0.1, far: 2000 }} style={{background: "#000000" }}>
-      
-        <Infos/>
-      
-        <Suspense fallback={<Loader/>}>
-          <ambientLight intensity={5}/>
+        <Suspense fallback={<Loader />}>
+          <ambientLight intensity={5} />
           {/* <spotLight position={[10, 15, 20]} angle={0.3}/>
           <hemisphereLight skyColor="#b1e1ff" groundColor="#000000" intensity={1}/> */}
 
-          <Satellite position={[2,2,2]} isRotating={true}/>
-          <Sky/>
-          {/* <Rock
+          <Satellite position={[2, 2, 2]} isRotating={true} />
+          <Sky />
+          <Rock
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
@@ -54,8 +57,8 @@ const Home = () => {
             setIsRotating={setIsRotating}
             // currentStage={currentStage}
             // setCurrentStage={setCurrentStage}
-            /> */}
-          <RockDetails
+          />
+          {/* <RockDetails
             position={islandPosition}
             scale={islandScale}
             rotation={islandRotation}
@@ -63,13 +66,11 @@ const Home = () => {
             // setIsRotating={setIsRotating}
             // currentStage={currentStage}
             // setCurrentStage={setCurrentStage}
-          />
-          
+          /> */}
         </Suspense>
       </Canvas>
     </section>
-  )
-}
+  );
+};
 
-
-export default Home
+export default Home;
